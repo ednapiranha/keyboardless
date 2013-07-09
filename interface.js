bde1cd99b8509a0267f9473cec55f4a7b2aca09e
Jen Fong-Adwent <jen@ednapiranha.com>
2013-07-09 18:57:58 -0400

    randomize button!

--- interface.js
+++ interface.js
@@ -1,38 +1,49 @@
 var word = new Word();
-var adverbs = word.getResult(word.adverbs);
-var adjectives = word.getResult(word.adjectives);
-var verbs = word.getResult(word.verbs);
-
 var pictograph = new Pictograph();
-var nouns = pictograph.getResult(pictograph.nouns);
 
 var displayField = $('#display-field');
 
-for (var i = 0; i < adverbs.length; i ++) {
-  $('ul.adverbs').append('<li>' + adverbs[i] + '</li>');
-}
+var randomize = function () {
+  var adverbs = word.getResult(word.adverbs);
+  var adjectives = word.getResult(word.adjectives);
+  var verbs = word.getResult(word.verbs);
+  var nouns = pictograph.getResult(pictograph.nouns);
 
-for (var i = 0; i < adjectives.length; i ++) {
-  $('ul.adjectives').append('<li>' + adjectives[i] + '</li>');
-}
+  $('ul').empty();
 
-for (var i = 0; i < verbs.length; i ++) {
-  $('ul.verbs').append('<li>' + verbs[i] + '</li>');
-}
+  for (var i = 0; i < adverbs.length; i ++) {
+    $('ul.adverbs').append('<li>' + adverbs[i] + '</li>');
+  }
 
-for (var i = 0; i < nouns.length; i ++) {
-  $('ul.nouns').append('<li>' + nouns[i] + '</li>');
-}
+  for (var i = 0; i < adjectives.length; i ++) {
+    $('ul.adjectives').append('<li>' + adjectives[i] + '</li>');
+  }
 
-$('li').on('touchstart click', function (ev) {
-  ev.preventDefault();
+  for (var i = 0; i < verbs.length; i ++) {
+    $('ul.verbs').append('<li>' + verbs[i] + '</li>');
+  }
 
-  displayField.append($(ev.target).text() + ' ');
-});
+  for (var i = 0; i < nouns.length; i ++) {
+    $('ul.nouns').append('<li>' + nouns[i] + '</li>');
+  }
+};
 
+randomize();
 
 $('button').on('touchstart click', function (ev) {
   ev.preventDefault();
 
-  displayField.empty();
+  var self = $(ev.target);
+
+  if (self.hasClass('reset')) {
+    displayField.empty();
+  } else {
+    randomize();
+  }
+});
+
+$('ul').on('touchstart click', 'li', function (ev) {
+  ev.preventDefault();
+
+  displayField.append($(ev.target).text() + ' ');
 });
